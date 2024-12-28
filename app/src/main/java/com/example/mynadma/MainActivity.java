89cda6453170820f2d33550f83eb121b01d35281
kalvin chen <kalvin412@gmail.com>
2024-12-29 05:47:24 +0800

    firebase setup

--- app/src/main/java/com/example/mynadma/MainActivity.java
+++ app/src/main/java/com/example/mynadma/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mynadma;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +9,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.FirebaseDatabase;
 
+public class MainActivity extends AppCompatActivity {
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        TextView dbStat = findViewById(R.id.dbStat);
+        if(db != null) {
+            dbStat.setText("connection successful");
+        } else {
+            dbStat.setText("connection failed");
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
