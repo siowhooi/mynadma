55f95eff39dbe893a1f55d492c1c10232cd139e5
siowhooi <choisiowhooi@gmail.com>
2025-01-17 13:17:02 +0800

    Your commit message

---
+++ app/src/main/java/com/example/mynadma/Profile.java
@@ -0,0 +1,20 @@
+package com.example.mynadma;
+
+public class Profile {
+
+    private String label;
+    private String value;
+
+    public Profile(String label, String value) {
+        this.label = label;
+        this.value = value;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
